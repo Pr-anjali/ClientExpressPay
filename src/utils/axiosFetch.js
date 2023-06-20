ed6b4ec94486cba4ebbb422267761f0b88f30322
Pranjali <cpranjali02@gmail.com>
2023-06-20 19:00:48 +0530

    Changes made

--- src/utils/axiosFetch.js
+++ src/utils/axiosFetch.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosFetch  = axios.create({
-  baseURL: "https://server-express-pay.vercel.app/",
+  baseURL: "https://server-express-pay-houy.vercel.app/",
   withCredentials: true,
 });
 
